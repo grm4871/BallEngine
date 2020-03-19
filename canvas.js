10f4bb142a72d8c5a0985c30470de097c5353978
grm4871 <grm4871@rit.edu>
2020-03-19 18:59:34 -0400

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -18,61 +18,107 @@ window.addEventListener('keydown', function(event) {
     console.log(event);
     if (event.keyCode == 37 && !left) {
         left = true;
-        Circles[0].addforcex(-10);
+        //Circles[0].addforcex(-10);
     }
     if (event.keyCode == 38 && !up) {
         up = true;
-        Circles[0].addforcey(-10);
+        //Circles[0].addforcey(-10);
     }
     if (event.keyCode == 39 && !right) {
         right = true;
-        Circles[0].addforcex(10)
+        //Circles[0].addforcex(10)
     }
     if (event.keyCode == 40 && !down) {
         down = true;
-        Circles[0].addforcey(10);
+        for(var c of Circles) {
+            console.log(c.fx);
+            console.log(c.fy);    
+        }
+        
+        //Circles[0].addforcey(10);
     }
     if (event.keyCode == 32) {
-        Circles.push(new Circle(30, 100, 100, 0, 0, 69, .99));
+        var rad = Math.random()*50
+        Circles.push(new Circle(rad, 100, 100, 10));
     }
 });
 
 window.addEventListener('keyup', function(event) {
     if (event.keyCode == 37 && left) {
-        Circles[0].addforcex(10);
+        //Circles[0].addforcex(10);
         left = false;
     }
     if (event.keyCode == 38 && up) {
-        Circles[0].addforcey(10);
+        //Circles[0].addforcey(10);
         up = false;
     }
     if (event.keyCode == 39 && right) {
-        Circles[0].addforcex(-10)
+        //Circles[0].addforcex(-10)
         right = false;
     }
     if (event.keyCode == 40 && down) {
-        Circles[0].addforcey(-10);
+        //Circles[0].addforcey(-10);
         down = false;
     }
 });
 
 class Circle {
-    constructor(radius, x, y, vx, vy, m, airres) {
+    constructor(radius, x, y, f) {
         this.radius = radius;
         this.x = x;
         this.y = y;
-        this.vx = vx;
-        this.vy = vy;
-        this.m = m;
+        this.vx = 0;
+        this.vy = 0;
+        this.m = 160;
         this.fx = 0;
         this.fy = 0;
-        this.airres = airres;
+        this.airres = .995;
+        this.force = f;
     }
     draw() {
         c.beginPath()
         c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
         c.strokeStyle = 'cyan';
         c.stroke();
+    }
+    setforce() { 
+        var nearest = null; 
+        var neardist;
+        for (var food of Foods) {
+            var d = (food.x-this.x)*(food.x-this.x) + (food.y-this.y)*(food.y-this.y)
+            if(nearest==null) {
+                nearest = food;
+                neardist = d;
+            }
+            else if(d < neardist) {
+                neardist = d;
+                nearest = food;
+            }
+        }
+
+        if(nearest) {
+            //draw line between
+            //c.beginPath();
+            //c.strokeStyle = 'white';
+            //c.moveTo(this.x, this.y);
+            //c.lineTo(nearest.x, nearest.y);
+            //c.stroke();        
+
+            //set force
+            neardist = Math.sqrt(neardist);
+            this.fx = (nearest.x - this.x)*this.force / neardist;
+            this.fy = (nearest.y - this.y)*this.force / neardist;
+            c.beginPath();
+            c.strokeStyle = 'white';
+            c.moveTo(this.x, this.y);
+            c.lineTo(this.x+(this.fx*10), this.y+(this.fy*10));
+            c.stroke();
+        }
+        else{
+            this.fx = 0;
+            this.fy = 0;
+        }
+
     }
     update(Circles) {
         //check for outside bounds
@@ -90,7 +136,7 @@ class Circle {
         }
         if (this.y-this.radius<0) {
             this.vy = -this.vy;            
-           this.y=this.radius;
+            this.y=this.radius;
         }
         for(const circle of Circles) {
             var distsq = (Math.pow(this.x-circle.x,2) + Math.pow(this.y-circle.y,2));
@@ -147,6 +193,13 @@ class Circle {
                 
             }
         }
+        for(const f in Foods) {
+            var food = Foods[f];
+            var distsq = (Math.pow(this.x-food.x,2) + Math.pow(this.y-food.y,2));
+            if(distsq < Math.pow((this.radius + food.radius),2)) {
+                Foods.splice(f,1);
+            }
+        }
         //move based on kinematics
         this.x += this.vx;
         this.y += this.vy;
@@ -154,31 +207,60 @@ class Circle {
         this.vx = this.airres * this.vx;
         this.vy += this.fy / this.m;
         this.vy = this.airres * this.vy;
+        
+        c.beginPath();
+        c.strokeStyle = 'yellow';
+        c.moveTo(this.x, this.y);
+        c.lineTo(this.x+(this.vx*10), this.y+(this.vy*10));
+        c.stroke();
+
 
         //if velocity small, stop
-        if (Math.abs(this.vx) < .05) { this.vx = 0;}
-        if (Math.abs(this.vy) < .05) { this.vy = 0;}
+        if (Math.abs(this.vx) < .05 && this.fx == 0) { this.vx = 0;}
+        if (Math.abs(this.vy) < .05 && this.fy == 0) { this.vy = 0;}
+        this.setforce();
         this.draw();
     }
-    addforcex(force) {
-        this.fx += force;
-    }
-    addforcey(force) {
-        this.fy += force;
-    }
-
 }
 
+class Food {
+    constructor(x, y) {
+        this.radius = 2;
+        this.x = x;
+        this.y = y;
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+        c.strokeStyle = 'green';
+        c.stroke();
+    }
+}
+
+
 let Circles = [];
-Circles.push(new Circle(30, 100, 100, 0, 0, 69, .99));
+Circles.push(new Circle(30, 100, 100, 10));
+
+let Foods = [];
+
+t = Date.now();
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, cw, ch);
     for (i=1;i<stepsPerFrame;i++) {
-        for (circle in Circles) {
-            Circles[circle].update(Circles);
+        for (circle of Circles) {
+            circle.update(Circles);
         }       
+        for (food of Foods) {
+            food.draw();
+        }
+    }
+    dt = Date.now() - t;
+
+    if(dt > 2000) {
+        Foods.push(new Food(Math.random()*cw, Math.random()*ch))
+        t = Date.now();
     }
 }
 
